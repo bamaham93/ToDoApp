4da2925486997e100553c43a6674d8bba8c79608
Jacob McGowin <bamaham93@gmail.com>
2021-08-11 23:21:34 -0400

    Simulated database results for dev use
    Added example data as if it were from a database query to build out the
    front end.

--- app.py
+++ app.py
@@ -1,23 +1,40 @@
 #! usr/bin/env python3
 
 """
+Simple to do manager app for personal use and learning.
 """
 
 
 from flask import Flask, render_template, redirect, url_for
-from flask_sqlalchemy import SQLAlchemy
+#from flask_sqlalchemy import SQLAlchemy
 
 
 app = Flask(__name__)
 
-database_result = [["Install aileron","Not Done","Need help"], ["Fix WO for N4837A", "Not Done", "See JG"]]
+database_results_not_finished = [
+																	["Establish world peace", "Not Done", "Might need some help"],
+																	["Make billions of dollars", "In progress", "May need some startup capital; $100?"],
+																	["Be outragrously generous", "Not Done", "Waiting on task 2"]
+																]
+
+database_results_in_progress = [
+																	["Phase Inspection on N444AK", "In Progress", ""],
+																	["Phase inspection on N353TS", "In Progress", ""],
+																	["N241EW Annual", "In Progress", ""],
+																]
+
+database_results_finished = [
+															["Install aileron","Done","Need help"], ["Fix WO for N4837A", "Done", "See JG"],
+															["", "", ""],
+															["", "", ""],
+														]
 
 
 @app.route('/', methods=['GET'])
 def home():
 	"""
 	"""
-	return render_template("home.html", username="Jacob", todos=database_result)
+	return render_template("home.html", username="Jacob", done=database_results_finished, not_done=database_results_not_finished, in_progress=database_results_in_progress)
 
 
 @app.route('/add/<item>', methods=["GET"])
@@ -31,5 +48,3 @@ def add_item(item):
 
 if __name__ == '__main__':
 	app.run()
-	
-	"{?p;.juy6rt}"
