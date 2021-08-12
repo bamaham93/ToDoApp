192fb3def681459e3a0ba4fa906244888f204c52
Jacob McGowin <bamaham93@gmail.com>
2021-08-11 23:33:53 -0400

    Outlined Database class
    Created methods that will be required for CRUD operations, plus a
    wrapper method around SQLite3's con.commit

--- database.py
+++ database.py
@@ -3,6 +3,8 @@
 """
 """
 
+import sqlite3
+
 
 class Database:
 	
@@ -10,4 +12,40 @@ class Database:
 	def __init__(self):
 		"""
 		"""
-		pass
+		self.con = sqlite3.connect("todo.db")
+		self.c = con.cursor()
+		self.results = None
+
+	
+	def create_item(self, item_name: str):
+		"""
+		"""
+		self.c.execute()
+		self._commit()
+	
+	
+	def read_items(self):
+		"""
+		"""
+		self.c.execute()
+	
+	
+	def update(self):
+		"""
+		"""
+		self.c.execute()
+		self._commit()
+	
+	
+	def delete(self):
+		"""
+		"""
+		self.c.execute()
+		self._commit()
+	
+	
+	def _commit(self):
+		"""
+		Commits changes; wrapper to comply with DRY principle.
+		"""
+		self.con.commit()
